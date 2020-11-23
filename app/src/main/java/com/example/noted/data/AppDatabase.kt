2873edea12c14dac570cb873c7a5ec1abce5cc36
mlssjblue <mlsusheel@gmail.com>
2020-11-23 19:08:35 +0800

    Remove unused imports

--- app/src/main/java/com/example/noted/data/AppDatabase.kt
+++ app/src/main/java/com/example/noted/data/AppDatabase.kt
@@ -4,11 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import com.example.noted.utilities.DATABASE_NAME
-import com.example.noted.workers.SeedDatabaseWorker
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
